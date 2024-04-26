a5132d9213aa52bd8d234a9ee1b678808103b25f
Diniduprabha <168133603+Diniduprabha@users.noreply.github.com>
2024-04-26 12:50:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -221,11 +221,12 @@ Wᴇʟᴄᴏᴍᴇ ᴛᴏ {chat}.
 ꜱʜᴀʀᴇ & ꜱᴜᴩᴩᴏʀᴛ, ʀᴇqᴜᴇꜱᴛ ʏᴏᴜ ᴡᴀɴᴛᴇᴅ ᴍᴏᴠɪᴇꜱ"""
   
     IMDB_TEMPLATE = """<b>Qᴜᴇʀʏ: {query}</b>
-
+    
 ☘️ Tɪᴛʟᴇ ➜ <a href={url}>{title}</a>
 🌼 Gᴇɴʀᴇꜱ ➜ {genres}
 📆 Yᴇᴀʀ ➜ <a href={url}/releaseinfo>{year}</a>
-🌟 Rᴀᴛɪɴɢ ➜ <a href={url}/ratings>{rating}</a>/10""" 📕 Lᴀɴɢᴜᴀɢᴇꜱ ➜ <code>{languages}</code>
+🌟 Rᴀᴛɪɴɢ ➜ <a href={url}/ratings>{rating}</a>/10""" 
+📕 Lᴀɴɢᴜᴀɢᴇꜱ ➜ <code>{languages}</code>
 ⏰ Rᴜɴᴛɪᴍᴇ ➜ {runtime} Minutes
 🌼 Gᴇɴʀᴇꜱ ➜ {genres}
 
